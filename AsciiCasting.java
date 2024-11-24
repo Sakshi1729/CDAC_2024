cadedfc532879ba631e27a17395241eacded8dee
Sakshi1729 <109221924+Sakshi1729@users.noreply.github.com>
2024-11-24 15:09:51 +0530

    first commit to CDAC_2024 Repo

---
+++ AsciiCasting.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class TestAscii
+{
+	public static void main(String args[])
+	{
+		int i,j;
+
+		for(i=65;i<=70;i++)
+		{
+			System.out.println((char)i);
+		}
+
+		System.out.println();
+	}
+}
